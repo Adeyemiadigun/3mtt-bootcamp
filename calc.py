7facbfa81565d9507d2c5881771d89382918d937
Adeyemiadigun <adeyemiadigun12@gmail.com>
2024-09-06 13:19:57 +0100

    first assignment

---
+++ calc.py
@@ -0,0 +1,17 @@
+a= int(input('Enter first num: '))
+b= int(input('Enter second num: '))
+def add():
+  result=a+b
+  print(result)
+  
+def subtract():
+  result=a-b
+  print(result)  
+
+def division():
+  result=a/b
+  print(result)  
+  
+subtract()  
+division()
+add()
